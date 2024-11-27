ce092c2d7464be823dfdb2fa3788ce2b92acc949
miguel977 <miguel645@gmail.com>
2024-11-27 07:15:54 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html')
     return render(request, 'recipes/home.html', context={
         'name':'Luiz Otavio',
     })
 
-def contato(request):
-    return render(request, 'recipes/contato.html')
 
-
-def sobre(request):
-    return HttpResponse("sobre")
