5941968a6a3ddc8d0b164d6cc122a7fa122dca97
miguel977 <miguel645@gmail.com>
2024-11-27 07:42:30 -0300

    Adicionando font-awesome

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
+from recipes.views import contato, home, sobre
 from recipes.views import home
 
-
-
 urlpatterns = [
-    path('', home ),
-    
+    path('', home),  # Home
+    path('sobre/', sobre),  # /sobre/
+    path('contato/', contato),  # /contato/
+    path('', home),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
 
 def home(request):
     return render(request, 'recipes/home.html', context={
-        'name':'Luiz Otavio',
+        'name': 'Luiz Otávio',
     })
+def contato(request):
+    return render(request, 'recipes/contato.html')
+
+def sobre(request):
+    return HttpResponse('sobre')
 
 
