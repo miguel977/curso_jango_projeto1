f0bdfa8f4d942530fe93a4474dc3236edf95bc86
miguel977 <miguel645@gmail.com>
2024-12-02 09:32:52 -0300

    separando os templates de home e recipes

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,10 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Otávio',
     })
+
+
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Luiz Otávio',
     })
+
