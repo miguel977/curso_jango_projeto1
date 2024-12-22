2ae9003cc7ea3fcd9afafde6543967641e0ad924
miguel977 <miguel645@gmail.com>
2024-12-22 08:23:42 -0300

    Criada aplicação recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
+def home(request):
+    return  HttpResponse('home')
+
+def sobre(request):
+    return  HttpResponse('sobre')
+
+def contato(request):
+    return  HttpResponse('contato')
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), # Home
+    path('sobre', sobre), # /sobre/
+    path('contato', contato),# /contato/
 ]
