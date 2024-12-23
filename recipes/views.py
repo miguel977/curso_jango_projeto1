e7a904c130d89df038c87d1ca06a09f0a7f33f84
miguel977 <miguel645@gmail.com>
2024-12-23 08:53:00 -0300

    exemplo de tempate temp

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recipes </title>
-</head>
-<body>
-    <h1>Recipes</h1>
-</body>
-</html>
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Luiz Otavio',
+    })
+
+
+def contato(request):
+    return render(request,'me_apague/temp.html')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
